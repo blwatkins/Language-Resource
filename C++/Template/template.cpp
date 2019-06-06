9be7d37084347e3da10c84c4859d6f9dbf7bfb91
Brittni Watkins <blwatkins@smu.edu>
2019-06-06 17:21:15 -0500

    Add template c++ program file.

---
+++ C++/Template/template.cpp
@@ -0,0 +1,16 @@
+// Template C++ Program
+
+// Single Line Comment
+
+/*
+Multi-line
+Comment
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+    return 0;
+}
