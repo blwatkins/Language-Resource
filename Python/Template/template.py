d5eba922584fdebd7ad3c1e4e5551f5188f68ce6
Brittni Watkins <blwatkins@smu.edu>
2019-08-07 16:55:25 -0500

    Create template program for python. Close #1

---
+++ Python/Template/template.py
@@ -0,0 +1,21 @@
+# !usr/bin/env python
+
+# Template Python Program
+
+# single line comment
+
+'''
+multi-line
+comment
+'''
+
+"""
+multi-line
+comment
+"""
+
+def main():
+    print('Hello, World.')
+
+if __name__ == '__main__':
+    main()
