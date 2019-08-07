a1d1017be67bf8f887b3711cf09d3e385d810d96
Brittni Watkins <blwatkins@smu.edu>
2019-08-07 10:42:11 -0500

    Add Java directory.  Add gitignore for class files. Add template program.

---
+++ Java/Template/Template.java
@@ -0,0 +1,16 @@
+// Template Java Program
+
+// Single line comment
+
+/*
+Multi-line
+comment
+*/
+
+class Template {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World.");
+    }
+
+}
