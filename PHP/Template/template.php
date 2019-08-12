e0db2c24477564b4175206b3d2ba85717eb1a38c
Brittni Watkins <blwatkins@smu.edu>
2019-08-11 21:25:29 -0500

    Add PHP template. Close #3

---
+++ PHP/Template/template.php
@@ -0,0 +1,21 @@
+<?php
+
+// Template PHP Program
+
+// single line comment
+
+# single line comment
+
+/*
+multi-line
+comment
+*/
+
+
+main();
+
+function main() {
+    echo('Hello, World.');
+}
+
+?>
