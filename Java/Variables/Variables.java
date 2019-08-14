f8678e0ae93a4f97eb4e23a67fcb2deacb0a1563
Brittni Watkins <blwatkins@smu.edu>
2019-08-14 17:06:27 -0500

    Add variable declarations and assignments for java. #5

---
+++ Java/Variables/Variables.java
@@ -0,0 +1,25 @@
+// Variables
+
+class Variables {
+
+    public static void main(String[] args) {
+        int x; // variable declaration
+        x = 7; // variable initialization/assignment
+
+        byte b = 50; //-128 to 127
+        short s = 450; //-32,768 to 32,767
+        int i = 34; //-(2^31) to (2^31)-1
+        long l = 5633042; //-(2^63) to (2^63)-1
+
+        float f = 56.345f; 
+        double d = 45.30;
+
+        boolean myBool = true;
+        
+        char c = 'x';
+        String myString = "This is my string";
+
+        System.out.println("The value of f is " + f);
+    }
+
+}
