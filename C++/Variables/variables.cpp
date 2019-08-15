71b54d9e0ca8be1f519b063bce92be8c2344c3fb
Brittni Watkins <blwatkins@smu.edu>
2019-08-15 14:28:25 -0500

    Complete c++ variables program. #5

--- C++/Variables/variables.cpp
+++ C++/Variables/variables.cpp
@@ -15,21 +15,32 @@ int main(int argc, char** argv) {
     int a {5634};
     int b = {}; // initialize to 0
     int c {}; // initialize to 0
-
-    c {65};
+    c = {65};
 
     short s = 34;
     int i = 50;
-    long l = 464234;
-    long long ll = -456456334539;
+    long l = 464234L;
+    long long ll = -456456334539LL;
 
-    unsigned short us = 453;
-    unsigned int ui = 435;
-    unsigned long ul = 634535;
-    unsigned long long ull = 346345354253;
+    unsigned short us = 453U;
+    unsigned int ui = 435U;
+    unsigned long ul = 634535UL;
+    unsigned long long ull = 346345354253UL;
+
+    float f = 342.54645F;
+    float science = 2.5E-4F;
+    double d = 23423.453;
+    long double ld = 43545.456L;
+
+    char myChar {'C'};
+    string myString {"This is my string"};
+
+    bool myBool {false};
 
     // full list of climits variables available at <http://www.cplusplus.com/reference/climits/>
     int maxInt = INT_MAX; // from the climits header file
+
+    cout << "The value of ul is " << ul << endl;
 
     return 0;
 }
