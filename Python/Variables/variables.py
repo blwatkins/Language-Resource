41434eb72629eb36c94c6d9c060a20912cf6cf33
Brittni Watkins <blwatkins@smu.edu>
2019-08-17 07:26:37 -0500

    Add variables file for Python. #5

---
+++ Python/Variables/variables.py
@@ -0,0 +1,13 @@
+# !usr/bin/env python
+
+# Variables
+
+def main():
+    myInt = 45
+    myFloat = 49.234
+    myString = 'This is my string'
+    myOtherString = "This is my other string"
+    print('myInt =', myInt)
+
+if __name__ == '__main__':
+    main()
