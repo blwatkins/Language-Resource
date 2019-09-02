62543b24df6792f5337d1f813889bbb1f269f121
Brittni Watkins <blwatkins@smu.edu>
2019-09-02 10:29:09 -0500

    Add mathematical expression examples for ruby.

---
+++ Ruby/Math/math.rb
@@ -0,0 +1,20 @@
+addition = 4 + 5
+puts "4 + 5 = #{addition}"
+
+subtraction = 4 - 5
+puts "4 - 5 = #{subtraction}"
+
+multiplication = 4 * 5
+puts "4 * 5 = #{multiplication}"
+
+division = 4 / 5 # integer division
+puts "4 / 5 = #{division}"
+
+division = 4.0 / 5.0 # floating point division
+puts "4.0 / 5.0 = #{division}"
+
+exponentation = 4 ** 5
+puts "4 ** 5 = #{exponentation}"
+
+modulus = 4 % 5
+puts "4 % 5 = #{modulus}"
