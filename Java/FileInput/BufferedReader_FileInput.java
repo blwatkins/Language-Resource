a3d77fbf314577384cab48c135dd04df02320dc6
Brittni Watkins <blwatkins@smu.edu>
2019-10-09 14:08:13 -0500

    Rename file input file to reflect method of input.

--- Java/FileInput/BufferedReader_FileInput.java
+++ Java/FileInput/BufferedReader_FileInput.java
@@ -4,7 +4,7 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 
-public class FileInput {
+public class BufferedReader_FileInput {
 
     public static void main(String[] args) {
         String fileName = "hope.txt";
