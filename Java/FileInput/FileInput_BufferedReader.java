44489f0f5514c5a7404739882caff7f726f6ef67
Brittni Watkins <blwatkins@smu.edu>
2019-10-09 14:18:03 -0500

    Rename buffered reader file input

--- Java/FileInput/FileInput_BufferedReader.java
+++ Java/FileInput/FileInput_BufferedReader.java
@@ -4,7 +4,7 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 
-public class BufferedReader_FileInput {
+public class FileInput_BufferedReader {
 
     public static void main(String[] args) {
         String fileName = "hope.txt";
