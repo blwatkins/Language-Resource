246676107aaed75478da2d4d3db8ddf18ca45adf
Brittni Watkins <blwatkins@smu.edu>
2019-11-08 12:44:54 -0600

    Add boolean expression examples.

---
+++ Ruby/Conditionals/boolean.rb
@@ -0,0 +1,8 @@
+# Boolean Operators
+
+has_ingredients = true
+has_time = false
+
+is_baking = has_ingredients && has_time
+maybe_baking = has_ingredients || has_time
+is_shopping = !has_ingredients
