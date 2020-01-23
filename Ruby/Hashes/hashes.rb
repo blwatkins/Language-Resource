98e429924a0690c715b6f64b638c1d90f9419cfb
Brittni Watkins <blwatkins@smu.edu>
2020-01-23 15:22:00 -0600

    Add examples from Ruby lesson on Hashes.

--- Ruby/Hashes/hashes.rb
+++ Ruby/Hashes/hashes.rb
@@ -1,3 +1,22 @@
 # Hashes
 # A collection of key-value pairs
 
+my_hash = { 
+	'name' => 'Eric',
+  	'age' => 26,
+  	'hungry?' => true
+}
+
+puts my_hash['name']
+puts my_hash['age']
+puts my_hash['hungry?']
+
+second_hash = Hash.new # empty hash
+second_hash['phone_number'] = '1234567890'
+second_hash['name'] = 'Bob'
+
+# Iteration over a Hash
+
+second_hash.each do |x, y|
+	puts "#{x}: #{y}"
+end
