9b5b825b624823a088fa1d2f2f672dd2991481ef
Brittni Watkins <blwatkins@smu.edu>
2020-01-24 14:51:12 -0600

    Add conditional assignment example.

--- Ruby/Variables/variables.rb
+++ Ruby/Variables/variables.rb
@@ -5,3 +5,16 @@ my_string = "Ruby"
 # output values
 puts my_num # puts -> adds a new line
 print my_string # print -> does not add new line
+
+puts ''
+puts '-----------------------'
+
+# Conditional Assignment
+# Only set variable if it has no value
+
+my_string ||= 'Ruby is cool'
+puts my_string
+
+my_value = nil
+my_value ||= 45
+puts my_value
