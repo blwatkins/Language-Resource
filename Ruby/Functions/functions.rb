ad89ffc0bb696614fcdfce731f4e4d2e34c15273
Brittni Watkins <blwatkins@smu.edu>
2020-01-24 11:49:24 -0600

    Add information on ruby functions.

---
+++ Ruby/Functions/functions.rb
@@ -0,0 +1,20 @@
+# Functions
+
+def print_to_10
+    for i in 1..10
+        puts i
+    end
+end
+
+print_to_10
+
+puts '-----------------------'
+
+def add(a, b)
+    return a + b
+end
+
+value = add(15, 2901)
+puts value
+
+puts '-----------------------'
