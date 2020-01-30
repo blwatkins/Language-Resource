efaa6a13fa991540d3972c61568bc1a2a56d58d6
Brittni Watkins <blwatkins@smu.edu>
2020-01-30 14:07:18 -0600

    Add attr_reader, attr_writer, and attr_accessor examples.

--- Ruby/Classes/classes.rb
+++ Ruby/Classes/classes.rb
@@ -2,15 +2,22 @@
 # Methods are public by default
 
 class Dog
+    attr_reader :weight # automatic getter
+    attr_writer :age # automatic setter
+    attr_accessor :breed # automatic getter and setter
 
     def initiailize # constructor
         @name = "Spot" #instance variable
         @age = 1;
+        @breed = "Golden Retriever"
+        @weight = 5;
     end
 
     def initialize(name) # parameterized constructor
         @name = name
         @age = 1
+        @breed = "Golden Retriever"
+        @weight = 5;
     end
 
     public # marks public methods
@@ -26,5 +33,9 @@ class Dog
 end
 
 dog = Dog.new("Carl")
+dog.age = 15 # age is writeable, but not readable
+dog.breed = "Shitzu" # breed is writeable and readable
 
 puts dog.name
+puts dog.weight # weight is readable, but not writeable
+puts dog.breed # breed is writeable and readable
