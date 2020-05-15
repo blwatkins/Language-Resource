276019ccf7a238925c505cdcc02df2e554430b28
Brittni Watkins <blwatkins@smu.edu>
2020-05-15 15:16:30 -0500

    Add getFinalGrade test

--- Java/_Libraries/JUnit/TestExample/src/test/grade/FinalGradeTest.java
+++ Java/_Libraries/JUnit/TestExample/src/test/grade/FinalGradeTest.java
@@ -1,7 +1,18 @@
 package test.grade;
 
+import org.junit.jupiter.api.Test;
+
+import grade.FinalGrade;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 
 class FinalGradeTest {
 
+    @Test
+    public void testGetFinalGrade(){
+        FinalGrade finalGrade = new FinalGrade();
+        String grade = finalGrade.getFinalGrade(90);
+        assertEquals("A", grade);
+    }
 }
