e02f52e9b275073e266944c272cfb7aedf2ecc2b
Brittni Watkins <blwatkins@smu.edu>
2020-05-15 15:31:20 -0500

    Add examples of assertion statements.

--- Java/_Libraries/JUnit/AssertionStatements/src/MainTest.java
+++ Java/_Libraries/JUnit/AssertionStatements/src/MainTest.java
@@ -1,5 +1,60 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
+
+    @Test
+    public void testAssertEquals() {
+        int myInt = 7;
+        assertEquals(7, myInt);
+    }
+
+    @Test
+    public void testAssertNotEquals() {
+        int myInt = 7;
+        assertNotEquals(10, myInt);
+    }
+
+    @Test
+    public void testAssertArrayEquals() {
+        char[] a = "hello".toCharArray();
+        char[] b = "hello".toCharArray();
+        assertArrayEquals(a, b);
+    }
+
+    @Test
+    public void testAssertTrue() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void testAssertFalse() {
+        assertFalse(false);
+    }
+
+    @Test
+    public void testAssertNull() {
+        assertNull(null);
+    }
+
+    @Test
+    public void testAssertNotNull() {
+        Object o = new Object();
+        assertNotNull(o);
+    }
+
+    @Test
+    public void testAssertSame() {
+        Object o = new Object();
+        assertSame(o, o);
+    }
+
+    @Test
+    public void testAssertNotSame() {
+        Object o1 = new Object();
+        Object o2 = new Object();
+        assertNotSame(o1, o2);
+    }
 
 }
