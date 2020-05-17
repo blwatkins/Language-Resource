b1d6514793083368d9d2a424046b0f94842802bd
Brittni Watkins <blwatkins@smu.edu>
2020-05-16 21:40:40 -0500

    update math for python 3.

--- Python/Math/math.py
+++ Python/Math/math.py
@@ -2,25 +2,25 @@
 # Arithmetic Operators
 
 a = 5 + 7 # addition
-print "5 + 7 = " + str(a)
+print("5 + 7 = " + str(a))
 
 b = 6 - 9 # subtraction
-print "6 - 9 = " + str(b)
+print("6 - 9 = " + str(b))
 
 c = 3 * 4 # multiplication
-print "3 * 4 = " + str(c)
+print("3 * 4 = " + str(c))
 
-d = 10 / 3 # integer division
-print "10 / 3 = " + str(d)
+d = 10 // 3 # integer division
+print("10 // 3 = " + str(d))
 
-e = 10.0 / 3.0 # floating point division
-print "10.0 / 3.0 = " + str(e)
+e = 10 / 3 # floating point division
+print("10 / 3 = " + str(e))
 
 f = 10 % 3 # modulus
-print "10 % 3 = " + str(f)
+print("10 % 3 = " + str(f))
 
 g = 5 ** 4 # exponentiation
-print "5 ** 4 = " + str(g)
+print("5 ** 4 = " + str(g))
 
 # augmented assignment
 x = 0
