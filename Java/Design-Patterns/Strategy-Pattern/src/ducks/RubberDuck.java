ead4c2c4c872a6dbe636519c8d51b8bb054752ce
Brittni Watkins <blwatkins@smu.edu>
2020-05-22 12:28:13 -0500

    Add constructor

--- Java/Design-Patterns/Strategy-Pattern/src/ducks/RubberDuck.java
+++ Java/Design-Patterns/Strategy-Pattern/src/ducks/RubberDuck.java
@@ -1,4 +1,12 @@
 package ducks;
 
+import behavior.fly.NoFly;
+import behavior.noise.SqueakNoise;
+
 public class RubberDuck extends Duck {
+
+    public RubberDuck() {
+        setFlyBehavior(new NoFly());
+        setNoiseBehavior(new SqueakNoise());
+    }
 }
