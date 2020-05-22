1e0ccfc56b3a93c93a4111448d3911d313892e0b
Brittni Watkins <blwatkins@smu.edu>
2020-05-22 12:27:37 -0500

    Add constructor

--- Java/Design-Patterns/Strategy-Pattern/src/ducks/MallardDuck.java
+++ Java/Design-Patterns/Strategy-Pattern/src/ducks/MallardDuck.java
@@ -1,4 +1,12 @@
 package ducks;
 
+import behavior.fly.WingFly;
+import behavior.noise.QuackNoise;
+
 public class MallardDuck extends Duck {
+
+    public MallardDuck() {
+        setFlyBehavior(new WingFly());
+        setNoiseBehavior(new QuackNoise());
+    }
 }
