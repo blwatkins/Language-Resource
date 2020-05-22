16698c05330532ae899aa54d7e99afe4f4ae7d28
Brittni Watkins <blwatkins@smu.edu>
2020-05-22 12:11:36 -0500

    Add FlyBehavior instance variable

--- Java/Design-Patterns/Strategy-Pattern/src/ducks/Duck.java
+++ Java/Design-Patterns/Strategy-Pattern/src/ducks/Duck.java
@@ -1,6 +1,9 @@
 package ducks;
 
+import behavior.fly.FlyBehavior;
+
 public abstract class Duck {
+    private FlyBehavior flyBehavior;
 
     public void swim() {
         System.out.println("I am swimming!");
