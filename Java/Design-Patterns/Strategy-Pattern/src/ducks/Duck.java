a9dd748260c0d26f1650afa83491161c704e32a2
Brittni Watkins <blwatkins@smu.edu>
2020-05-22 12:14:06 -0500

    Add constructor for flyBehavior Add fly method

--- Java/Design-Patterns/Strategy-Pattern/src/ducks/Duck.java
+++ Java/Design-Patterns/Strategy-Pattern/src/ducks/Duck.java
@@ -5,7 +5,15 @@ import behavior.fly.FlyBehavior;
 public abstract class Duck {
     private FlyBehavior flyBehavior;
 
+    public Duck(FlyBehavior flyBehavior) {
+        this.flyBehavior = flyBehavior;
+    }
+
     public void swim() {
         System.out.println("I am swimming!");
+    }
+
+    public void fly() {
+        flyBehavior.fly();
     }
 }
