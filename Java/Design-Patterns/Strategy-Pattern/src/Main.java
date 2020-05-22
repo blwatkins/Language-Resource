d4f0658482acd743597e52e67bda36bfdc744d2d
Brittni Watkins <blwatkins@smu.edu>
2020-05-22 12:34:12 -0500

    Make Duck simulator main program

--- Java/Design-Patterns/Strategy-Pattern/src/Main.java
+++ Java/Design-Patterns/Strategy-Pattern/src/Main.java
@@ -1,2 +1,24 @@
+import ducks.*;
+
 public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Decoy Duck!");
+        Duck decoy = new DecoyDuck();
+        performActions(decoy);
+
+        System.out.println("Mallard Duck!");
+        Duck mallard = new MallardDuck();
+        performActions(mallard);
+
+        System.out.println("Rubber Duck!");
+        Duck rubber = new RubberDuck();
+        performActions(rubber);
+    }
+
+    private static void performActions(Duck duck) {
+        duck.fly();
+        duck.makeNoise();
+        duck.swim();
+    }
 }
