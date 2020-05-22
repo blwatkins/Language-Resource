553083560d2144780c91b8f52f3d942a5cb1a6ab
Brittni Watkins <blwatkins@smu.edu>
2020-05-22 12:26:08 -0500

    Add setters for flyBehavior and noiseBehavior

--- Java/Design-Patterns/Strategy-Pattern/src/ducks/Duck.java
+++ Java/Design-Patterns/Strategy-Pattern/src/ducks/Duck.java
@@ -7,6 +7,14 @@ public abstract class Duck {
     private FlyBehavior flyBehavior;
     private NoiseBehavior noiseBehavior;
 
+    public void setFlyBehavior(FlyBehavior flyBehavior) {
+        this.flyBehavior = flyBehavior;
+    }
+
+    public void setNoiseBehavior(NoiseBehavior noiseBehavior) {
+        this.noiseBehavior = noiseBehavior;
+    }
+
     public void swim() {
         System.out.println("I am swimming!");
     }
