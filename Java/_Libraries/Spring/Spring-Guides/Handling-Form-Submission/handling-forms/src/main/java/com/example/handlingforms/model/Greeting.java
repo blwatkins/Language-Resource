e2ca14f8277199f1dd1cf36a1d87ccb376b4c523
Brittni Watkins <blwatkins@smu.edu>
2020-05-22 12:56:00 -0500

    Add instance variables to Greeting class

--- Java/_Libraries/Spring/Spring-Guides/Handling-Form-Submission/handling-forms/src/main/java/com/example/handlingforms/model/Greeting.java
+++ Java/_Libraries/Spring/Spring-Guides/Handling-Form-Submission/handling-forms/src/main/java/com/example/handlingforms/model/Greeting.java
@@ -1,4 +1,22 @@
 package com.example.handlingforms.model;
 
 public class Greeting {
+    private long id;
+    private String content;
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
 }
