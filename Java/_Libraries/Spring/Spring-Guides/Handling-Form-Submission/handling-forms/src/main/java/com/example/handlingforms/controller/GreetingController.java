230b9ff0e8beb61e33c7ee8f7052d4d3e16d0a7c
Brittni Watkins <blwatkins@smu.edu>
2020-05-22 12:53:19 -0500

    Add import statements for GreetingController Add Controller annotation

--- Java/_Libraries/Spring/Spring-Guides/Handling-Form-Submission/handling-forms/src/main/java/com/example/handlingforms/controller/GreetingController.java
+++ Java/_Libraries/Spring/Spring-Guides/Handling-Form-Submission/handling-forms/src/main/java/com/example/handlingforms/controller/GreetingController.java
@@ -1,4 +1,12 @@
 package com.example.handlingforms.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
 public class GreetingController {
+
 }
