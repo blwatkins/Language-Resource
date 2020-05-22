fbcab4c3dd4d520d928a90b969ea5e5abb369892
Brittni Watkins <blwatkins@smu.edu>
2020-05-22 12:18:01 -0500

    Remove Duck class constructor

--- Java/Design-Patterns/Strategy-Pattern/src/ducks/Duck.java
+++ Java/Design-Patterns/Strategy-Pattern/src/ducks/Duck.java
@@ -5,10 +5,6 @@ import behavior.fly.FlyBehavior;
 public abstract class Duck {
     private FlyBehavior flyBehavior;
 
-    public Duck(FlyBehavior flyBehavior) {
-        this.flyBehavior = flyBehavior;
-    }
-
     public void swim() {
         System.out.println("I am swimming!");
     }
