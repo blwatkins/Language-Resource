af76a4d43a4351eb6506b5fde9b3c4f4d354875f
Brittni Watkins <blwatkins@smu.edu>
2020-05-22 14:08:31 -0500

    Add data, constructor, getters, and setters to Customer class

--- Java/_Libraries/Spring/Spring-Guides/Accessing-Data-with-JDBC/relational-data/src/main/java/com/example/relationaldata/Customer.java
+++ Java/_Libraries/Spring/Spring-Guides/Accessing-Data-with-JDBC/relational-data/src/main/java/com/example/relationaldata/Customer.java
@@ -1,4 +1,41 @@
 package com.example.relationaldata;
 
 public class Customer {
+    private long id;
+    private String firstName;
+    private String lastName;
+
+    public Customer(long id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String toString() {
+        return String.format("Customer{id = %d, firstName = %s, lastName = %s}", id, firstName, lastName);
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 }
