4d26dabd3a1e736f1765d2f79b5e677398478c4a
Brittni Watkins <blwatkins@smu.edu>
2020-05-22 14:34:05 -0500

    Add mappings for web controller

--- Java/_Libraries/Spring/Spring-Guides/Validating-Form-Input/validating-form/src/main/java/com/example/validatingform/controller/WebController.java
+++ Java/_Libraries/Spring/Spring-Guides/Validating-Form-Input/validating-form/src/main/java/com/example/validatingform/controller/WebController.java
@@ -1,4 +1,36 @@
 package com.example.validatingform.controller;
 
-public class WebController {
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.example.validatingform.model.Person;
+
+
+@Controller
+public class WebController implements WebMvcConfigurer {
+
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/results").setViewName("results");
+    }
+
+    @GetMapping("/")
+    public String showForm(Person person) {
+        return "form";
+    }
+
+    @PostMapping("/")
+    public String checkPersonData(@Valid Person person, BindingResult result) {
+
+        if (result.hasErrors()) {
+            return "form";
+        } else {
+            return "redirect:/results";
+        }
+    }
 }
