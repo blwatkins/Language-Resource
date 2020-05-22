3c3c8f0158936c552e3066e8ac7e2f22936af96c
Brittni Watkins <blwatkins@smu.edu>
2020-05-22 12:36:45 -0500

    Separate object creation and printing into separate functions

--- Java/Design-Patterns/Strategy-Pattern/src/Main.java
+++ Java/Design-Patterns/Strategy-Pattern/src/Main.java
@@ -3,17 +3,30 @@ import ducks.*;
 public class Main {
 
     public static void main(String[] args) {
+        makeAndShowDecoyDuck();
+        makeAndShowMallardDuck();
+        makeAndShowRubberDuck();
+    }
+
+    private static void makeAndShowDecoyDuck() {
         System.out.println("Decoy Duck!");
         Duck decoy = new DecoyDuck();
         performActions(decoy);
+        System.out.println();
+    }
 
+    private static void makeAndShowMallardDuck() {
         System.out.println("Mallard Duck!");
         Duck mallard = new MallardDuck();
         performActions(mallard);
+        System.out.println();
+    }
 
+    private static void makeAndShowRubberDuck() {
         System.out.println("Rubber Duck!");
         Duck rubber = new RubberDuck();
         performActions(rubber);
+        System.out.println();
     }
 
     private static void performActions(Duck duck) {
