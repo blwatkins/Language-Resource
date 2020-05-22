00ec4d0be5f7747c47ac506e01cebebafd5c6f81
Brittni Watkins <blwatkins@smu.edu>
2020-05-22 12:26:59 -0500

    Add constructor

--- Java/Design-Patterns/Strategy-Pattern/src/ducks/DecoyDuck.java
+++ Java/Design-Patterns/Strategy-Pattern/src/ducks/DecoyDuck.java
@@ -1,4 +1,12 @@
 package ducks;
 
+import behavior.fly.NoFly;
+import behavior.noise.QuackNoise;
+
 public class DecoyDuck extends Duck {
+
+    public DecoyDuck() {
+        setFlyBehavior(new NoFly());
+        setNoiseBehavior(new QuackNoise());
+    }
 }
