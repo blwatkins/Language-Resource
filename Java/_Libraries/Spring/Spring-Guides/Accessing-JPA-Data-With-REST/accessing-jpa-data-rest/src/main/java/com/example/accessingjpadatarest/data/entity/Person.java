9eb1f05f8b8e90643363cb04a434dcec4a413de1
Brittni Watkins <blwatkins@smu.edu>
2020-05-23 13:23:47 -0500

    Add Person implementation

--- Java/_Libraries/Spring/Spring-Guides/Accessing-JPA-Data-With-REST/accessing-jpa-data-rest/src/main/java/com/example/accessingjpadatarest/data/entity/Person.java
+++ Java/_Libraries/Spring/Spring-Guides/Accessing-JPA-Data-With-REST/accessing-jpa-data-rest/src/main/java/com/example/accessingjpadatarest/data/entity/Person.java
@@ -1,4 +1,38 @@
 package com.example.accessingjpadatarest.data.entity;
 
+import javax.persistence.*;
+
+@Entity
 public class Person {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private long id;
+
+    private String firstName;
+    private String lastName;
+
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 }
