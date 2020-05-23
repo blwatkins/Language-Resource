3e1dfa07ca4e3cbf838266a791930528c91892ae
Brittni Watkins <blwatkins@smu.edu>
2020-05-23 17:33:28 -0500

    Add controller information for BasicController class

--- Java/_Libraries/Spring/Practice/spring-mvc/src/main/java/com/example/springmvc/controller/BasicController.java
+++ Java/_Libraries/Spring/Practice/spring-mvc/src/main/java/com/example/springmvc/controller/BasicController.java
@@ -1,4 +1,18 @@
 package com.example.springmvc.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/basic")
 public class BasicController {
+
+    @GetMapping
+    @ResponseBody
+    public String welcome() {
+        return "Welcome to this page!";
+    }
+
 }
