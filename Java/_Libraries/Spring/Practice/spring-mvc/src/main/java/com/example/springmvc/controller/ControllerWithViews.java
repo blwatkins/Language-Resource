b0d02e075612b2cab3037ddf69aaa0b25a650f29
Brittni Watkins <blwatkins@smu.edu>
2020-05-23 17:42:26 -0500

    Add Controller information Add get mapping for welcome-view

--- Java/_Libraries/Spring/Practice/spring-mvc/src/main/java/com/example/springmvc/controller/ControllerWithViews.java
+++ Java/_Libraries/Spring/Practice/spring-mvc/src/main/java/com/example/springmvc/controller/ControllerWithViews.java
@@ -1,4 +1,16 @@
 package com.example.springmvc.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/views")
 public class ControllerWithViews {
+
+    @GetMapping("/welcome-view")
+    public String welcome() {
+        return "welcome";
+    }
+
 }
