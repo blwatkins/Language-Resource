868dc631d2e3196cd3a91a41e6c02eedc7ab9a23
Brittni Watkins <blwatkins@smu.edu>
2020-05-23 13:28:01 -0500

    Change path to PersonRepository

--- Java/_Libraries/Spring/Spring-Guides/Accessing-JPA-Data-With-REST/accessing-jpa-data-rest/src/main/java/com/example/accessingjpadatarest/data/repository/PersonRepository.java
+++ Java/_Libraries/Spring/Spring-Guides/Accessing-JPA-Data-With-REST/accessing-jpa-data-rest/src/main/java/com/example/accessingjpadatarest/data/repository/PersonRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.example.accessingjpadatarest.data.entity.Person;
 
+@RepositoryRestResource(collectionResourceRel="people", path="people")
 public interface PersonRepository extends CrudRepository<Person, Long> {
     List<Person> findByLastName(String lastName);
 }
