d23b4a41a684055e67b5c5eb91d27179c42a5d23
Brittni Watkins <blwatkins@smu.edu>
2020-05-23 13:15:31 -0500

    Add initial files for Accessing JPA Data with REST example.

---
+++ Java/_Libraries/Spring/Spring-Guides/Accessing-JPA-Data-With-REST/accessing-jpa-data-rest/src/main/java/com/example/accessingjpadatarest/AccessingJpaDataRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.accessingjpadatarest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessingJpaDataRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccessingJpaDataRestApplication.class, args);
+	}
+
+}
