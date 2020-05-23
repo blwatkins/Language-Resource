478747677770382e4feabf27fde4d2e8214bcd79
Brittni Watkins <blwatkins@smu.edu>
2020-05-23 17:57:40 -0500

    Add GET mapping for simple model with request parameter

--- Java/_Libraries/Spring/Practice/spring-mvc/src/main/java/com/example/springmvc/controller/ControllerWithViews.java
+++ Java/_Libraries/Spring/Practice/spring-mvc/src/main/java/com/example/springmvc/controller/ControllerWithViews.java
@@ -1,8 +1,10 @@
 package com.example.springmvc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/views")
@@ -11,6 +13,12 @@ public class ControllerWithViews {
     @GetMapping("/welcome-view")
     public String welcome() {
         return "welcome";
+    }
+
+    @GetMapping("/welcome-model")
+    public String welcomeModel(@RequestParam(name="name", defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "welcome-model";
     }
 
 }
