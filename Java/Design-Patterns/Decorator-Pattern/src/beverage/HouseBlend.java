692cd3619d7b97ee2c25586a4899a87d031628de
Brittni Watkins <blwatkins@smu.edu>
2020-05-24 09:50:33 -0500

    Add methods to HouseBlend class

--- Java/Design-Patterns/Decorator-Pattern/src/beverage/HouseBlend.java
+++ Java/Design-Patterns/Decorator-Pattern/src/beverage/HouseBlend.java
@@ -1,4 +1,12 @@
 package beverage;
 
 public class HouseBlend {
+
+    public String getDescription() {
+        return "House Blend";
+    }
+
+    public double getCost() {
+        return 0.89;
+    }
 }
