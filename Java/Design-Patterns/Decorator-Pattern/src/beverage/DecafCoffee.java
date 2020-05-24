cd5635babf32113acd5c4c25c5837871583ecd0f
Brittni Watkins <blwatkins@smu.edu>
2020-05-24 09:53:56 -0500

    Implement methods for DecafCoffee Add inheritance from Beverage class

--- Java/Design-Patterns/Decorator-Pattern/src/beverage/DecafCoffee.java
+++ Java/Design-Patterns/Decorator-Pattern/src/beverage/DecafCoffee.java
@@ -1,4 +1,12 @@
 package beverage;
 
-public class DecafCoffee {
+public class DecafCoffee extends Beverage {
+
+    public String getDescription() {
+        return "Decaf Coffee";
+    }
+
+    public double getCost() {
+        return 1.05;
+    }
 }
