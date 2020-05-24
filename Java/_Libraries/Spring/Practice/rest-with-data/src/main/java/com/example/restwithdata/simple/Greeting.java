da2804fb447d5b878e560f60da9c53352833b1ef
Brittni Watkins <blwatkins@smu.edu>
2020-05-24 13:22:48 -0500

    Add message instance variable to Greeting class

--- Java/_Libraries/Spring/Practice/rest-with-data/src/main/java/com/example/restwithdata/simple/Greeting.java
+++ Java/_Libraries/Spring/Practice/rest-with-data/src/main/java/com/example/restwithdata/simple/Greeting.java
@@ -1,4 +1,13 @@
 package com.example.restwithdata.simple;
 
 public class Greeting {
+    private String message;
+
+    public Greeting(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
 }
