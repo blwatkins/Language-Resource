b49929aaa0842b8ebc9b53e14328413dd88a7d7f
Brittni Watkins <blwatkins@smu.edu>
2020-05-24 10:04:16 -0500

    Add inheritance from BeverageDecorator Add methods for Mocha class

--- Java/Design-Patterns/Decorator-Pattern/src/beverage/decorator/Mocha.java
+++ Java/Design-Patterns/Decorator-Pattern/src/beverage/decorator/Mocha.java
@@ -1,4 +1,18 @@
 package beverage.decorator;
 
-public class Mocha {
+import beverage.Beverage;
+
+public class Mocha extends BeverageDecorator {
+
+    public Mocha(Beverage beverage) {
+        super(beverage);
+    }
+
+    public String getDescription() {
+        return beverage.getDescription() + ", Mocha";
+    }
+
+    public double getCost() {
+        return beverage.getCost() + 0.20;
+    }
 }
