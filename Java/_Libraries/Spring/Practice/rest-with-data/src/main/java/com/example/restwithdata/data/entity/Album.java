b77203f4b718e7e1d9c1104e47e0043a243e3352
Brittni Watkins <blwatkins@smu.edu>
2020-05-24 13:52:00 -0500

    Define Album as an Entity Add instance variables

--- Java/_Libraries/Spring/Practice/rest-with-data/src/main/java/com/example/restwithdata/data/entity/Album.java
+++ Java/_Libraries/Spring/Practice/rest-with-data/src/main/java/com/example/restwithdata/data/entity/Album.java
@@ -1,4 +1,35 @@
 package com.example.restwithdata.data.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Album {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private long id;
+
+    private String name;
+
+    public Album(String name) {
+        this.name = name;
+    }
+
+    protected Album() {
+
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
