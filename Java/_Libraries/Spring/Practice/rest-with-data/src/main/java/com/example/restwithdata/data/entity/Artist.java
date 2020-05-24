2e1dcb48247a6b3be2870a32f0dda9cea67fe962
Brittni Watkins <blwatkins@smu.edu>
2020-05-24 14:11:37 -0500

    Fix whitespace in Artist class

--- Java/_Libraries/Spring/Practice/rest-with-data/src/main/java/com/example/restwithdata/data/entity/Artist.java
+++ Java/_Libraries/Spring/Practice/rest-with-data/src/main/java/com/example/restwithdata/data/entity/Artist.java
@@ -7,7 +7,7 @@ import javax.persistence.*;
 @Entity
 public class Artist {
     @Id
-    @GeneratedValue(strategy= GenerationType.AUTO)
+    @GeneratedValue(strategy=GenerationType.AUTO)
     private long id;
 
     @OneToMany(mappedBy="artist")
