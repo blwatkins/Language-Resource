ccdab43ec0310a972d2dd51d091ddc91953629ca
Brittni Watkins <blwatkins@smu.edu>
2020-05-24 13:40:03 -0500

    Declare Artist as an Entity Add instance variables to Artist class

--- Java/_Libraries/Spring/Practice/rest-with-data/src/main/java/com/example/restwithdata/data/entity/Artist.java
+++ Java/_Libraries/Spring/Practice/rest-with-data/src/main/java/com/example/restwithdata/data/entity/Artist.java
@@ -1,4 +1,32 @@
 package com.example.restwithdata.data.entity;
 
+import javax.persistence.*;
+
+@Entity
 public class Artist {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private long id;
+
+    private String name;
+
+    public Artist(String name) {
+        this.name = name;
+    }
+
+    protected Artist() {
+
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
