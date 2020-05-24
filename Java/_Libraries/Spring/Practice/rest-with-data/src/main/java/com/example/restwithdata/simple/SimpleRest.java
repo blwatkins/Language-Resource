deef2b6d4efc916e312143e32ad7273efa7c78eb
Brittni Watkins <blwatkins@smu.edu>
2020-05-24 13:32:52 -0500

    Add mappings to SimpleRest class

--- Java/_Libraries/Spring/Practice/rest-with-data/src/main/java/com/example/restwithdata/simple/SimpleRest.java
+++ Java/_Libraries/Spring/Practice/rest-with-data/src/main/java/com/example/restwithdata/simple/SimpleRest.java
@@ -1,7 +1,26 @@
 package com.example.restwithdata.simple;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/simple")
 public class SimpleRest {
+
+    @GetMapping("/greeting")
+    public Greeting helloWorld() {
+        return (new Greeting("Hello, World!"));
+    }
+
+    @GetMapping("/greeting/{name}")
+    public Greeting greetingWithName(@PathVariable String name) {
+        String message = "Hello, " + name + "!";
+        return (new Greeting(message));
+    }
+
+    @GetMapping("/greetingParam")
+    public Greeting greetingWithParam(@RequestParam(name="name", defaultValue="World") String name) {
+        String message = "Hello, " + name + "!";
+        return (new Greeting(message));
+    }
+
 }
