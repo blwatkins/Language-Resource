0c14a7b7bdc76b766186b26298260caa6bcf4625
Brittni Watkins <blwatkins@smu.edu>
2020-05-24 13:15:36 -0500

    Add inital files from REST with Data practice project.

---
+++ Java/_Libraries/Spring/Practice/rest-with-data/src/main/java/com/example/restwithdata/RestWithDataApplication.java
@@ -0,0 +1,13 @@
+package com.example.restwithdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestWithDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestWithDataApplication.class, args);
+	}
+
+}
