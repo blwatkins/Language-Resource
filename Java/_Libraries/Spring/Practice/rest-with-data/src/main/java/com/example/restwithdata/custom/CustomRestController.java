6703524a6cba6b860ba3b4f8157948a40eb9810e
Brittni Watkins <blwatkins@smu.edu>
2020-05-24 15:26:55 -0500

    Add mapping for custom controller

--- Java/_Libraries/Spring/Practice/rest-with-data/src/main/java/com/example/restwithdata/custom/CustomRestController.java
+++ Java/_Libraries/Spring/Practice/rest-with-data/src/main/java/com/example/restwithdata/custom/CustomRestController.java
@@ -1,4 +1,25 @@
 package com.example.restwithdata.custom;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.restwithdata.data.entity.Artist;
+import com.example.restwithdata.data.repository.ArtistRepository;
+
+@RestController
+@RequestMapping("/custom")
 public class CustomRestController {
+    private ArtistRepository artistRepository;
+
+    @Autowired
+    public CustomRestController(ArtistRepository artistRepository) {
+        this.artistRepository = artistRepository;
+    }
+
+    @GetMapping("/artists")
+    public Iterable<Artist> getAllArtists() {
+        return artistRepository.findAll();
+    }
 }
