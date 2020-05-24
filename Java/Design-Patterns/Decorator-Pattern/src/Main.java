fc31dbe778777bec28423aba8a2e7d7f2a1d615b
Brittni Watkins <blwatkins@smu.edu>
2020-05-24 10:11:14 -0500

    Add code for creating two decorated coffees

--- Java/Design-Patterns/Decorator-Pattern/src/Main.java
+++ Java/Design-Patterns/Decorator-Pattern/src/Main.java
@@ -1,6 +1,32 @@
+import beverage.*;
+import beverage.decorator.*;
+
 public class Main {
 
     public static void main(String[] args) {
+        makeHouseBlendMochaSoy();
+        makeDecafMocha();
+    }
 
+    private static void makeHouseBlendMochaSoy() {
+        Beverage houseBlend = new HouseBlend();
+        printInformation(houseBlend);
+        houseBlend = new Mocha(houseBlend);
+        printInformation(houseBlend);
+        houseBlend = new Soy(houseBlend);
+        printInformation(houseBlend);
+        System.out.println();
+    }
+
+    private static void makeDecafMocha() {
+        Beverage decaf = new DecafCoffee();
+        printInformation(decaf);
+        decaf = new Mocha(decaf);
+        printInformation(decaf);
+        System.out.println();
+    }
+
+    private static void printInformation(Beverage beverage) {
+        System.out.println(beverage.getDescription() + " = " + beverage.getCost());
     }
 }
