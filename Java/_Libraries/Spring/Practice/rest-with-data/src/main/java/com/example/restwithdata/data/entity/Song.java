75dcea15d8d9109cb8ed7adde0abbc6f4782ef19
Brittni Watkins <blwatkins@smu.edu>
2020-05-24 13:48:53 -0500

    Define Song as an entity. Add instance variables

--- Java/_Libraries/Spring/Practice/rest-with-data/src/main/java/com/example/restwithdata/data/entity/Song.java
+++ Java/_Libraries/Spring/Practice/rest-with-data/src/main/java/com/example/restwithdata/data/entity/Song.java
@@ -1,4 +1,42 @@
 package com.example.restwithdata.data.entity;
 
+import javax.persistence.*;
+
+@Entity
 public class Song {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private long id;
+
+    private String name;
+    private int trackNumber;
+
+    public Song(String name, int trackNumber) {
+        this.name = name;
+        this.trackNumber = trackNumber;
+    }
+
+    protected Song() {
+
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getTrackNumber() {
+        return trackNumber;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setTrackNumber(int trackNumber) {
+        this.trackNumber = trackNumber;
+    }
 }
