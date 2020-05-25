978b2bfcfbe3f0803259a077de179ea7228aaf08
Brittni Watkins <blwatkins@smu.edu>
2020-05-25 18:22:40 -0500

    Add PizzaIngredientFactory implementations

--- Java/Design-Patterns/Abstract-Factory-Pattern/src/factory/ChicagoStylePizzaIngredientFactory.java
+++ Java/Design-Patterns/Abstract-Factory-Pattern/src/factory/ChicagoStylePizzaIngredientFactory.java
@@ -1,4 +1,23 @@
 package factory;
 
-public class ChicagoStylePizzaIngredientFactory {
+import pizza.ingredients.cheese.Cheese;
+import pizza.ingredients.cheese.MozzarellaCheese;
+import pizza.ingredients.dough.Dough;
+import pizza.ingredients.dough.ThickCrustDough;
+import pizza.ingredients.pepperoni.Pepperoni;
+import pizza.ingredients.pepperoni.SpicyPepperoni;
+
+public class ChicagoStylePizzaIngredientFactory implements PizzaIngredientFactory {
+
+    public Cheese createCheese() {
+        return (new MozzarellaCheese());
+    }
+
+    public Dough createDough() {
+        return (new ThickCrustDough());
+    }
+
+    public Pepperoni createPepperoni() {
+        return (new SpicyPepperoni());
+    }
 }
--- Java/Design-Patterns/Abstract-Factory-Pattern/src/factory/NewYorkStylePizzaIngredientFactory.java
+++ Java/Design-Patterns/Abstract-Factory-Pattern/src/factory/NewYorkStylePizzaIngredientFactory.java
@@ -1,4 +1,23 @@
 package factory;
 
-public class NewYorkStylePizzaIngredientFactory {
+import pizza.ingredients.cheese.Cheese;
+import pizza.ingredients.cheese.ProvoloneCheese;
+import pizza.ingredients.dough.Dough;
+import pizza.ingredients.dough.ThinCrustDough;
+import pizza.ingredients.pepperoni.MildPepperoni;
+import pizza.ingredients.pepperoni.Pepperoni;
+
+public class NewYorkStylePizzaIngredientFactory implements PizzaIngredientFactory {
+
+    public Cheese createCheese() {
+        return (new ProvoloneCheese());
+    }
+
+    public Dough createDough() {
+        return (new ThinCrustDough());
+    }
+
+    public Pepperoni createPepperoni() {
+        return (new MildPepperoni());
+    }
 }
