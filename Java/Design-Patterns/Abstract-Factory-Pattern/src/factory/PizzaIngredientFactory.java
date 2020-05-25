bbe189eb89221a5e2844774c00b3ea7482748a1e
Brittni Watkins <blwatkins@smu.edu>
2020-05-25 18:16:39 -0500

    Add methods to PizzaIngredientFactory

--- Java/Design-Patterns/Abstract-Factory-Pattern/src/factory/PizzaIngredientFactory.java
+++ Java/Design-Patterns/Abstract-Factory-Pattern/src/factory/PizzaIngredientFactory.java
@@ -1,4 +1,11 @@
 package factory;
 
+import pizza.ingreedients.cheese.Cheese;
+import pizza.ingreedients.dough.Dough;
+import pizza.ingreedients.pepperoni.Pepperoni;
+
 public interface PizzaIngredientFactory {
+    Cheese createCheese();
+    Dough createDough();
+    Pepperoni createPepperoni();
 }
