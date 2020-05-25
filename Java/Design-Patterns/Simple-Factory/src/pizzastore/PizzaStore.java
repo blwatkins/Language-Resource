be4775f7c1da62da873c7b8f9808b8378d01b543
Brittni Watkins <blwatkins@smu.edu>
2020-05-25 17:28:05 -0500

    Add PizzaStore class

---
+++ Java/Design-Patterns/Simple-Factory/src/pizzastore/PizzaStore.java
@@ -0,0 +1,21 @@
+package pizzastore;
+
+import factory.PizzaFactory;
+import pizza.Pizza;
+
+public class PizzaStore {
+    PizzaFactory pizzaFactory;
+
+    public PizzaStore(PizzaFactory pizzaFactory) {
+        this.pizzaFactory = pizzaFactory;
+    }
+
+    public Pizza orderPizza(String type) {
+        Pizza pizza = pizzaFactory.createPizza(type);
+        pizza.prepare();
+        pizza.bake();
+        pizza.cut();
+        pizza.box();
+        return pizza;
+    }
+}
