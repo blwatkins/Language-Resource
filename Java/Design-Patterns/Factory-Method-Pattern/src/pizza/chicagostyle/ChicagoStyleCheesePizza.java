023f8dc581bdaec37d140964b48790cb0b9a4430
Brittni Watkins <blwatkins@smu.edu>
2020-05-25 17:42:24 -0500

    Add ChicagoStyleCheesePizza and ChicagoStylePepperoniPizza classes

---
+++ Java/Design-Patterns/Factory-Method-Pattern/src/pizza/chicagostyle/ChicagoStyleCheesePizza.java
@@ -0,0 +1,10 @@
+package pizza.chicagostyle;
+
+import pizza.Pizza;
+
+public class ChicagoStyleCheesePizza extends Pizza {
+
+    public ChicagoStyleCheesePizza() {
+        super("Chicago Style Cheese Pizza");
+    }
+}
---
+++ Java/Design-Patterns/Factory-Method-Pattern/src/pizza/chicagostyle/ChicagoStylePepperoniPizza.java
@@ -0,0 +1,10 @@
+package pizza.chicagostyle;
+
+import pizza.Pizza;
+
+public class ChicagoStylePepperoniPizza extends Pizza {
+
+    public ChicagoStylePepperoniPizza() {
+        super("Chicago Style Pepperoni Pizza");
+    }
+}
