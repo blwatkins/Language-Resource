e5abe9293687219edf6b3cc98b392417f8a067cc
Brittni Watkins <blwatkins@smu.edu>
2020-05-25 17:49:43 -0500

    Add createPizza method to ChicagoStylePizzaStore class Have ChicagoStylePizzaStore implement PizzaStore interface

--- Java/Design-Patterns/Factory-Method-Pattern/src/pizzastore/ChicagoStylePizzaStore.java
+++ Java/Design-Patterns/Factory-Method-Pattern/src/pizzastore/ChicagoStylePizzaStore.java
@@ -1,4 +1,19 @@
 package pizzastore;
 
-public class ChicagoStylePizzaStore {
+import pizza.Pizza;
+import pizza.chicagostyle.*;
+
+public class ChicagoStylePizzaStore extends PizzaStore {
+
+    public Pizza createPizza(String type) {
+        Pizza pizza;
+
+        if (type.equalsIgnoreCase("cheese")) {
+            pizza = new ChicagoStyleCheesePizza();
+        } else {
+            pizza = new ChicagoStylePepperoniPizza();
+        }
+
+        return pizza;
+    }
 }
