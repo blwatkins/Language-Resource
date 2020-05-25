cd259b33c03381f439c1598fa51b4a1546abf7ef
Brittni Watkins <blwatkins@smu.edu>
2020-05-25 17:17:12 -0500

    Add Pizza class methods

--- Java/Design-Patterns/Simple-Factory/src/pizza/Pizza.java
+++ Java/Design-Patterns/Simple-Factory/src/pizza/Pizza.java
@@ -1,4 +1,29 @@
 package pizza;
 
-public class Pizza {
+public abstract class Pizza {
+    private String name;
+
+    public Pizza(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void prepare() {
+        System.out.println("Preparing " + name);
+    }
+
+    public void bake() {
+        System.out.println("Baking for 25 minutes at 350");
+    }
+
+    public void cut() {
+        System.out.println("Cutting into triangle slices");
+    }
+
+    public void box() {
+        System.out.println("Placing pizza in a box");
+    }
 }
