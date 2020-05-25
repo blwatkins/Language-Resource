311e77c19041c231548d4177292205759cde3109
Brittni Watkins <blwatkins@smu.edu>
2020-05-25 17:47:40 -0500

    Add create pizza method to NewYorkStylePizzaStore class

--- Java/Design-Patterns/Factory-Method-Pattern/src/pizzastore/NewYorkStylePizzaStore.java
+++ Java/Design-Patterns/Factory-Method-Pattern/src/pizzastore/NewYorkStylePizzaStore.java
@@ -1,4 +1,19 @@
 package pizzastore;
 
-public class NewYorkStylePizzaStore {
+import pizza.Pizza;
+import pizza.newyorkstyle.*;
+
+public class NewYorkStylePizzaStore extends PizzaStore {
+
+    public Pizza createPizza(String type) {
+        Pizza pizza;
+
+        if (type.equalsIgnoreCase("cheese")) {
+            pizza = new NewYorkStyleCheesePizza();
+        } else {
+            pizza = new NewYorkStylePepperoniPizza();
+        }
+
+        return pizza;
+    }
 }
