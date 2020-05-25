1fcfb53b8f8b4b83680537f515075cbc8ab4269a
Brittni Watkins <blwatkins@smu.edu>
2020-05-25 17:30:04 -0500

    Add pizza creation to Main

--- Java/Design-Patterns/Simple-Factory/src/Main.java
+++ Java/Design-Patterns/Simple-Factory/src/Main.java
@@ -1,6 +1,13 @@
+import factory.PizzaFactory;
+import pizzastore.PizzaStore;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        PizzaFactory pizzaFactory = new PizzaFactory();
+        PizzaStore pizzaStore = new PizzaStore(pizzaFactory);
+        pizzaStore.orderPizza("Cheese");
+        System.out.println();
+        pizzaStore.orderPizza("Pepperoni");
     }
 }
