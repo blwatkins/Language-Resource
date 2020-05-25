3b9452843096badca3206feaabd0c43a932d3124
Brittni Watkins <blwatkins@smu.edu>
2020-05-25 18:04:32 -0500

    Have MozzarellaCheese class implement Cheese interface

--- Java/Design-Patterns/Abstract-Factory-Pattern/src/pizza/ingreedients/cheese/MozzarellaCheese.java
+++ Java/Design-Patterns/Abstract-Factory-Pattern/src/pizza/ingreedients/cheese/MozzarellaCheese.java
@@ -1,4 +1,8 @@
 package pizza.ingreedients.cheese;
 
-public class MozzarellaCheese {
+public class MozzarellaCheese extends Cheese {
+
+    public MozzarellaCheese() {
+        super("Mozzarella Cheese");
+    }
 }
