4d4fe6b15869b2f091142906038b4956f50acc95
Brittni Watkins <blwatkins@smu.edu>
2020-05-25 18:41:29 -0500

    Add pizza creation to main method

--- Java/Design-Patterns/Abstract-Factory-Pattern/src/Main.java
+++ Java/Design-Patterns/Abstract-Factory-Pattern/src/Main.java
@@ -1,6 +1,17 @@
+import pizzastore.ChicagoStylePizzaStore;
+import pizzastore.NewYorkStylePizzaStore;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        NewYorkStylePizzaStore newYorkPizzaStore = new NewYorkStylePizzaStore();
+        ChicagoStylePizzaStore chicagoPizzaStore = new ChicagoStylePizzaStore();
+        newYorkPizzaStore.orderPizza("cheese");
+        System.out.println();
+        newYorkPizzaStore.orderPizza("pepperoni");
+        System.out.println();
+        chicagoPizzaStore.orderPizza("cheese");
+        System.out.println();
+        chicagoPizzaStore.orderPizza("pepperoni");
     }
 }
