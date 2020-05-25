e5c4c20301cb2e525176871cc3334e5822e96da2
Brittni Watkins <blwatkins@smu.edu>
2020-05-25 17:25:17 -0500

    Add Pizza creation method

--- Java/Design-Patterns/Simple-Factory/src/factory/PizzaFactory.java
+++ Java/Design-Patterns/Simple-Factory/src/factory/PizzaFactory.java
@@ -1,4 +1,20 @@
 package factory;
 
+import pizza.*;
+
 public class PizzaFactory {
+
+    public Pizza createPizza(String type) {
+        Pizza pizza;
+
+        if (type.equalsIgnoreCase("Cheese")) {
+            pizza = new CheesePizza();
+        } else if (type.equalsIgnoreCase("Pepperoni")) {
+            pizza = new PepperoniPizza();
+        } else {
+            pizza = new VeggiePizza();
+        }
+
+        return pizza;
+    }
 }
