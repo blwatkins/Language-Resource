cc2d5df94666d87b8301a534244b7f094efbeb27
Brittni Watkins <blwatkins@smu.edu>
2020-05-25 17:40:28 -0500

    Add NewYorkStylePepperoniPizza class

---
+++ Java/Design-Patterns/Factory-Method-Pattern/src/pizza/newyorkstyle/NewYorkStylePepperoniPizza.java
@@ -0,0 +1,10 @@
+package pizza.newyorkstyle;
+
+import pizza.Pizza;
+
+public class NewYorkStylePepperoniPizza extends Pizza {
+
+    public NewYorkStylePepperoniPizza() {
+        super("New York Style Pepperoni Pizza");
+    }
+}
