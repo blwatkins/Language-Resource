35c9f457f8cd1f3d23b80e0508686a6c89550bbb
Brittni Watkins <blwatkins@smu.edu>
2020-05-26 08:29:00 -0500

    Add ChocolateMixer creation code to main

--- Java/Design-Patterns/Singleton-Pattern/src/Main.java
+++ Java/Design-Patterns/Singleton-Pattern/src/Main.java
@@ -1,6 +1,11 @@
+import singleton.ChocolateMixer;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        ChocolateMixer chocolateMixer = ChocolateMixer.getInstance();
+        chocolateMixer.fill();
+        chocolateMixer.boil();
+        chocolateMixer.drain();
     }
 }
