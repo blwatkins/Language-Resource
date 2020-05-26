32983603f7240cff594e9ac553cd5248b4eab53f
Brittni Watkins <blwatkins@smu.edu>
2020-05-26 08:31:27 -0500

    Add initial test file for ChocolateMixer

---
+++ Java/Design-Patterns/Singleton-Pattern/test/singleton/ChocolateMixerTest.java
@@ -0,0 +1,12 @@
+package singleton;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ChocolateMixerTest {
+
+    @Test
+    void getInstance() {
+    }
+}
