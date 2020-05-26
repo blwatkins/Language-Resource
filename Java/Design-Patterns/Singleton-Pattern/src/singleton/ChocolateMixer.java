4ce6669e610d878da2420b9e30b11ef09b6210b1
Brittni Watkins <blwatkins@smu.edu>
2020-05-26 08:26:39 -0500

    Add ChocolateMixer methods

--- Java/Design-Patterns/Singleton-Pattern/src/singleton/ChocolateMixer.java
+++ Java/Design-Patterns/Singleton-Pattern/src/singleton/ChocolateMixer.java
@@ -1,4 +1,54 @@
 package singleton;
 
 public class ChocolateMixer {
+    private boolean empty;
+    private boolean boiled;
+    private ChocolateMixer chocolateMixer;
+
+    private ChocolateMixer() {
+        empty = true;
+        boiled = false;
+    }
+
+    public synchronized ChocolateMixer getInstance() {
+
+        if (chocolateMixer == null) {
+            chocolateMixer = new ChocolateMixer();
+        }
+
+        return chocolateMixer;
+    }
+
+    public void fill() {
+
+        if (isEmpty()) {
+            empty = false;
+            boiled = false;
+            System.out.println("Filling the mixer");
+        }
+    }
+
+    public void drain() {
+
+        if (!isEmpty() && isBoiled()) {
+            empty = true;
+            System.out.println("Draining the boiled mixture");
+        }
+    }
+
+    public void boil() {
+
+        if (!isEmpty() && !isBoiled()) {
+            boiled = true;
+            System.out.println("Boiling the mixture");
+        }
+    }
+
+    public boolean isEmpty() {
+        return empty;
+    }
+
+    public boolean isBoiled() {
+        return boiled;
+    }
 }
