c8b3e06d895f449951ba2dce689022f95e1fcca2
Brittni Watkins <blwatkins@smu.edu>
2020-05-27 10:14:09 -0500

    Add appliances to Main class

--- Java/Design-Patterns/Command-Pattern/src/Main.java
+++ Java/Design-Patterns/Command-Pattern/src/Main.java
@@ -1,6 +1,13 @@
+import appliance.GarageDoor;
+import appliance.Light;
+import appliance.Sprinkler;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        Light kitchenLight = new Light("Kitchen");
+        Light livingRoomLight = new Light("Living Room");
+        GarageDoor garageDoor = new GarageDoor();
+        Sprinkler sprinkler = new Sprinkler();
     }
 }
