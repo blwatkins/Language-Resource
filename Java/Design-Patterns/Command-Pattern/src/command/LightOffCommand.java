8e6f436379041e8bbee8c046007bd9affc11465b
Brittni Watkins <blwatkins@smu.edu>
2020-05-27 09:52:07 -0500

    Add LightOffCommand class

---
+++ Java/Design-Patterns/Command-Pattern/src/command/LightOffCommand.java
@@ -0,0 +1,19 @@
+package command;
+
+import appliance.Light;
+
+public class LightOffCommand implements Command {
+    Light light;
+
+    public LightOffCommand(Light light) {
+        this.light = light;
+    }
+
+    public void execute() {
+        light.turnOff();
+    }
+
+    public void undo() {
+        light.turnOn();
+    }
+}
