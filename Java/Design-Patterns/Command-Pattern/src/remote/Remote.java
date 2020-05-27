7c06558e341144f4c237142b56bcdc3be3c50f27
Brittni Watkins <blwatkins@smu.edu>
2020-05-27 10:11:46 -0500

    Add methods and instance variables to Remote class

--- Java/Design-Patterns/Command-Pattern/src/remote/Remote.java
+++ Java/Design-Patterns/Command-Pattern/src/remote/Remote.java
@@ -1,4 +1,54 @@
 package remote;
 
+import command.Command;
+import command.NoCommand;
+
 public class Remote {
+    private final int numberOfSlots;
+    private Command[] onCommands;
+    private Command[] offCommands;
+    private Command undoCommand;
+
+    public Remote() {
+        numberOfSlots = 4;
+        onCommands = new Command[numberOfSlots];
+        offCommands = new Command[numberOfSlots];
+        Command noCommand = new NoCommand();
+        undoCommand = noCommand;
+
+        for (int i = 0; i < numberOfSlots; i++) {
+            onCommands[i] = noCommand;
+            offCommands[i] = noCommand;
+        }
+    }
+
+    public void setCommand(int slot, Command onCommand, Command offCommand) {
+            if (validButton(slot)) {
+                onCommands[slot] = onCommand;
+                offCommands[slot] = offCommand;
+            }
+    }
+
+    public void onButton(int slot) {
+        if (validButton(slot)) {
+            onCommands[slot].execute();
+            undoCommand = onCommands[slot];
+        }
+    }
+
+    public void offButton(int slot) {
+        if (validButton(slot)) {
+            offCommands[slot].execute();
+            undoCommand = offCommands[slot];
+        }
+    }
+
+    public void undoButton() {
+        undoCommand.undo();
+    }
+
+    private boolean validButton(int slot) {
+        return slot < numberOfSlots;
+    }
+
 }
