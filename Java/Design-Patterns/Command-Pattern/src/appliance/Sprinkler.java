39b02ebe23fc4fda57b5ae6bb87c603b839fff26
Brittni Watkins <blwatkins@smu.edu>
2020-05-27 09:47:26 -0500

    Add Sprinkler class

---
+++ Java/Design-Patterns/Command-Pattern/src/appliance/Sprinkler.java
@@ -0,0 +1,12 @@
+package appliance;
+
+public class Sprinkler {
+
+    public void waterOn() {
+        System.out.println("The sprinklers are on");
+    }
+
+    public void waterOff() {
+        System.out.println("The sprinklers are off");
+    }
+}
