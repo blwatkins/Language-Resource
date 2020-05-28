3dfa3fc12c9cc8b1b08a2c2d00599d7980ce518b
Brittni Watkins <blwatkins@smu.edu>
2020-05-28 12:58:15 -0500

    Add Duck and TurkeyDuck creation to Main class

--- Java/Design-Patterns/Adapter-Pattern/src/Main.java
+++ Java/Design-Patterns/Adapter-Pattern/src/Main.java
@@ -1,6 +1,19 @@
+import adapter.TurkeyDuck;
+import duck.Duck;
+import duck.MallardDuck;
+import turkey.WildTurkey;
+
 public class Main {
 
     public static void main(String[] args) {
+        Duck mallard = new MallardDuck();
+        Duck turkeyDuck = new TurkeyDuck(new WildTurkey());
+        performDuckActions(mallard);
+        performDuckActions(turkeyDuck);
+    }
 
+    private static void performDuckActions(Duck duck) {
+        duck.quack();
+        duck.fly();
     }
 }
