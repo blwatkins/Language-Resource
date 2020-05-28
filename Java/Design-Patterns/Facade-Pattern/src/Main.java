0ce78c8429cb430759ba0638edf9325e7c7e220c
Brittni Watkins <blwatkins@smu.edu>
2020-05-28 13:19:03 -0500

    Add HomeTheaterFacade creation to Main class

--- Java/Design-Patterns/Facade-Pattern/src/Main.java
+++ Java/Design-Patterns/Facade-Pattern/src/Main.java
@@ -1,6 +1,11 @@
+import facade.HomeTheaterFacade;
+import theater.*;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        HomeTheaterFacade homeTheaterFacade = new HomeTheaterFacade(new DvdPlayer(), new Light(), new PopcornMachine(), new Screen());
+        homeTheaterFacade.watchMovie("How to Lose a Guy in 10 Days");
+        homeTheaterFacade.endMovie();
     }
 }
