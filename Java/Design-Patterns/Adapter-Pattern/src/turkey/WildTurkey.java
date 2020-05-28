8d718fa416c8c222a6f1c794b95ab84eae521d7e
Brittni Watkins <blwatkins@smu.edu>
2020-05-28 12:51:40 -0500

    Add WildTurkey class

---
+++ Java/Design-Patterns/Adapter-Pattern/src/turkey/WildTurkey.java
@@ -0,0 +1,12 @@
+package turkey;
+
+public class WildTurkey implements Turkey {
+
+    public void gobble() {
+        System.out.println("Gobble");
+    }
+
+    public void fly() {
+        System.out.println("The turkey is flying");
+    }
+}
