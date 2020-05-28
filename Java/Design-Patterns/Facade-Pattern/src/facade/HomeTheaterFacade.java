990df719e12ea15ee95362bc640f5dd24246fc38
Brittni Watkins <blwatkins@smu.edu>
2020-05-28 13:13:17 -0500

    Add initial HomeTheaterFacade class

---
+++ Java/Design-Patterns/Facade-Pattern/src/facade/HomeTheaterFacade.java
@@ -0,0 +1,10 @@
+package facade;
+
+import theater.*;
+
+public class HomeTheaterFacade {
+    private DvdPlayer dvdPlayer;
+    private Light light;
+    private PopcornMachine popcornMachine;
+    private Screen screen;
+}
