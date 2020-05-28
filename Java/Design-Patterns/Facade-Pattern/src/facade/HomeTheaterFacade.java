1f211bdd6e6ec77719ce759fa3fbfd75c03dbf2e
Brittni Watkins <blwatkins@smu.edu>
2020-05-28 13:16:58 -0500

    Add constructor and methods to HomeTheaterFacade class

--- Java/Design-Patterns/Facade-Pattern/src/facade/HomeTheaterFacade.java
+++ Java/Design-Patterns/Facade-Pattern/src/facade/HomeTheaterFacade.java
@@ -7,4 +7,29 @@ public class HomeTheaterFacade {
     private Light light;
     private PopcornMachine popcornMachine;
     private Screen screen;
+
+    public HomeTheaterFacade(DvdPlayer dvdPlayer, Light light, PopcornMachine popcornMachine, Screen screen) {
+        this.dvdPlayer = dvdPlayer;
+        this.light = light;
+        this.popcornMachine = popcornMachine;
+        this.screen = screen;
+    }
+
+    public void watchMovie(String movie) {
+        popcornMachine.on();
+        popcornMachine.pop();
+        light.off();
+        screen.down();
+        dvdPlayer.on();
+        dvdPlayer.insertDvd(movie);
+        dvdPlayer.play();
+    }
+
+    public void endMovie() {
+        popcornMachine.off();
+        light.on();
+        screen.up();
+        dvdPlayer.ejectDvd();
+        dvdPlayer.off();
+    }
 }
