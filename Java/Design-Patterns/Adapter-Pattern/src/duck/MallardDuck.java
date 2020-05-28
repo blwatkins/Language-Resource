a0e4ec606a01aa47f38db01162dd954271c07492
Brittni Watkins <blwatkins@smu.edu>
2020-05-28 12:49:37 -0500

    Add MallardDuck class

---
+++ Java/Design-Patterns/Adapter-Pattern/src/duck/MallardDuck.java
@@ -0,0 +1,12 @@
+package duck;
+
+public class MallardDuck implements Duck {
+
+    public void quack() {
+        System.out.println("Quack!");
+    }
+
+    public void fly() {
+        System.out.println("The duck is flying!");
+    }
+}
