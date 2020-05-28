6f8871926e774cca9f86a738df5eae88732506fc
Brittni Watkins <blwatkins@smu.edu>
2020-05-28 13:08:53 -0500

    Add Screen class

---
+++ Java/Design-Patterns/Facade-Pattern/src/theater/Screen.java
@@ -0,0 +1,12 @@
+package theater;
+
+public class Screen {
+
+    public void up() {
+        System.out.println("Screen is rising.");
+    }
+
+    public void down() {
+        System.out.println("Screen is lowering!");
+    }
+}
