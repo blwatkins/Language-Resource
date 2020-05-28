471dd53190fdcaaa2eb36babb223048ed1f8193e
Brittni Watkins <blwatkins@smu.edu>
2020-05-28 13:03:51 -0500

    Add PopcornMachine class

---
+++ Java/Design-Patterns/Facade-Pattern/src/theater/PopcornMachine.java
@@ -0,0 +1,16 @@
+package theater;
+
+public class PopcornMachine {
+
+    public void on() {
+        System.out.println("Turning on popcorn machine!");
+    }
+
+    public void off() {
+        System.out.println("Turning off popcorn machine.");
+    }
+
+    public void pop() {
+        System.out.println("Popping popcorn!");
+    }
+}
