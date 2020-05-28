d85173249ef459a5453452b20d595f4418d5ca42
Brittni Watkins <blwatkins@smu.edu>
2020-05-28 12:59:15 -0500

    Add punctuation to WildTurkey output statements

--- Java/Design-Patterns/Adapter-Pattern/src/turkey/WildTurkey.java
+++ Java/Design-Patterns/Adapter-Pattern/src/turkey/WildTurkey.java
@@ -3,10 +3,10 @@ package turkey;
 public class WildTurkey implements Turkey {
 
     public void gobble() {
-        System.out.println("Gobble");
+        System.out.println("Gobble!");
     }
 
     public void fly() {
-        System.out.println("The turkey is flying");
+        System.out.println("The turkey is flying!");
     }
 }
