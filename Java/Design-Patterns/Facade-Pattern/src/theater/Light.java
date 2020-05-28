ed07e182b3c9c32102b86efcf4cff014d94ac978
Brittni Watkins <blwatkins@smu.edu>
2020-05-28 13:10:02 -0500

    Add Light class

---
+++ Java/Design-Patterns/Facade-Pattern/src/theater/Light.java
@@ -0,0 +1,12 @@
+package theater;
+
+public class Light {
+
+    public void on() {
+        System.out.println("Turning lights on.");
+    }
+
+    public void off() {
+        System.out.println("Turning lights off!");
+    }
+}
