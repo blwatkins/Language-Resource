5a4cf8f6337eeb455f650b32138a6e1b802a1e57
Brittni Watkins <blwatkins@smu.edu>
2020-05-29 11:34:10 -0500

    Add methods and instance variables to QuestionAsker class

--- Java/Design-Patterns/Template-Method-Pattern/src/userresponse/QuestionAsker.java
+++ Java/Design-Patterns/Template-Method-Pattern/src/userresponse/QuestionAsker.java
@@ -1,4 +1,30 @@
 package userresponse;
 
+import java.util.Scanner;
+
 public class QuestionAsker {
+    private String question;
+    private String answer;
+    Scanner input;
+
+    public QuestionAsker(String question) {
+        input = new Scanner(System.in);
+        this.question = question;
+        this.answer = "";
+    }
+
+    public void askQuestion() {
+        System.out.print(question + " ");
+        answer =  input.nextLine();
+    }
+
+    public boolean answerIs(String answer) {
+        boolean answerMatches = false;
+
+        if (this.answer.equalsIgnoreCase(answer)) {
+            answerMatches = true;
+        }
+
+        return answerMatches;
+    }
 }
