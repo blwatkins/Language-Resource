25e623486b3737e1584f44397ca30ddd2d9ac5f6
Brittni Watkins <blwatkins@smu.edu>
2020-05-29 11:36:36 -0500

    Add coffee and tea creation to main class

--- Java/Design-Patterns/Template-Method-Pattern/src/Main.java
+++ Java/Design-Patterns/Template-Method-Pattern/src/Main.java
@@ -1,6 +1,12 @@
+import beverage.*;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        Beverage tea = new Tea();
+        Beverage coffee = new Coffee();
+        tea.prepare();
+        System.out.println();
+        coffee.prepare();
     }
 }
