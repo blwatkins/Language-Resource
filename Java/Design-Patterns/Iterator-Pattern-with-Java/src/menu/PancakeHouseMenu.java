9a2e49aa86cf8b90f1c7f7d4e2ede214cc6883db
Brittni Watkins <blwatkins@smu.edu>
2020-05-30 12:19:52 -0500

    Have PancakeHouseMenu class implement Menu interface Add instance variables and constructor to PancakeHouseMenu class

--- Java/Design-Patterns/Iterator-Pattern-with-Java/src/menu/PancakeHouseMenu.java
+++ Java/Design-Patterns/Iterator-Pattern-with-Java/src/menu/PancakeHouseMenu.java
@@ -1,4 +1,26 @@
 package menu;
 
-public class PancakeHouseMenu {
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+
+public class PancakeHouseMenu implements Menu {
+    private List<MenuItem> menuItems;
+
+    public PancakeHouseMenu() {
+        menuItems = new ArrayList<>();
+        addItem("Pancakes", 2.99);
+        addItem("Blueberry Pancakes", 3.49);
+        addItem("Waffles", 3.59);
+    }
+
+    public Iterator<MenuItem> createIterator() {
+        return menuItems.iterator();
+    }
+
+    private void addItem(String name, double price) {
+        MenuItem menuItem = new MenuItem(name, price);
+        menuItems.add(menuItem);
+    }
 }
