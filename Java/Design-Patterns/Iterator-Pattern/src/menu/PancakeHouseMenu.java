b9f2280608d50cd100edc69e2e8be012e48f5600
Brittni Watkins <blwatkins@smu.edu>
2020-05-30 11:27:37 -0500

    Add menuItems attribute and constructor to PancakeHouseMenu class

--- Java/Design-Patterns/Iterator-Pattern/src/menu/PancakeHouseMenu.java
+++ Java/Design-Patterns/Iterator-Pattern/src/menu/PancakeHouseMenu.java
@@ -1,4 +1,20 @@
 package menu;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PancakeHouseMenu {
+    List<MenuItem> menuItems;
+
+    public PancakeHouseMenu() {
+        menuItems = new ArrayList<>();
+        addItem("Pancakes", 2.99);
+        addItem("Blueberry Pancakes", 3.49);
+        addItem("Waffles", 3.59);
+    }
+
+    private void addItem(String name, double price) {
+        MenuItem menuItem = new MenuItem(name, price);
+        menuItems.add(menuItem);
+    }
 }
