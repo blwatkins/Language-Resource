8326bd3e071c005934635336bce241b89f691edd
Brittni Watkins <blwatkins@smu.edu>
2020-05-30 11:33:27 -0500

    Add instance variables and constructor to DinerMenu class

--- Java/Design-Patterns/Iterator-Pattern/src/menu/DinerMenu.java
+++ Java/Design-Patterns/Iterator-Pattern/src/menu/DinerMenu.java
@@ -1,4 +1,24 @@
 package menu;
 
 public class DinerMenu {
+    static final int MAX_ITEMS = 6;
+    int numberOfItems;
+    MenuItem[] menuItems;
+
+    public DinerMenu() {
+        menuItems = new MenuItem[MAX_ITEMS];
+        numberOfItems = 0;
+        addItem("BLT", 2.99);
+        addItem("Soup of the Day", 3.29);
+        addItem("Hotdog", 3.05);
+    }
+
+    private void addItem(String name, double price) {
+
+        if (numberOfItems < MAX_ITEMS) {
+            MenuItem menuItem = new MenuItem(name, price);
+            menuItems[numberOfItems] = menuItem;
+            numberOfItems++;
+        }
+    }
 }
