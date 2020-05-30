0cbb4373f48f8a28d1cba796df854df146c04752
Brittni Watkins <blwatkins@smu.edu>
2020-05-30 12:58:16 -0500

    Add Waitress class

---
+++ Java/Design-Patterns/Composite-Pattern/src/waitress/Waitress.java
@@ -0,0 +1,15 @@
+package waitress;
+
+import menu.MenuComponent;
+
+public class Waitress {
+    MenuComponent allMenus;
+
+    public Waitress(MenuComponent allMenus) {
+        this.allMenus = allMenus;
+    }
+
+    public void printMenus() {
+        allMenus.print();
+    }
+}
