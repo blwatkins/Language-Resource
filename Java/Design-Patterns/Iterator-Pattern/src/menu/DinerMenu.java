eb0c5f4bee4acead4003a26fb5d1221941e6088f
Brittni Watkins <blwatkins@smu.edu>
2020-05-30 11:48:21 -0500

    Have DinerMenu implement Menu interface

--- Java/Design-Patterns/Iterator-Pattern/src/menu/DinerMenu.java
+++ Java/Design-Patterns/Iterator-Pattern/src/menu/DinerMenu.java
@@ -1,6 +1,9 @@
 package menu;
 
-public class DinerMenu {
+import iterator.DinerMenuIterator;
+import iterator.Iterator;
+
+public class DinerMenu implements Menu {
     static final int MAX_ITEMS = 6;
     int numberOfItems;
     MenuItem[] menuItems;
@@ -11,6 +14,10 @@ public class DinerMenu {
         addItem("BLT", 2.99);
         addItem("Soup of the Day", 3.29);
         addItem("Hot Dog", 3.05);
+    }
+
+    public Iterator createIterator() {
+        return (new DinerMenuIterator(menuItems));
     }
 
     private void addItem(String name, double price) {
