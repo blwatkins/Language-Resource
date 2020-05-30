ef22b84264bc6ba1173c9fae8c904c42656e24b3
Brittni Watkins <blwatkins@smu.edu>
2020-05-30 12:13:56 -0500

    Have DinerMenuIterator implement Iterator interface. Add instance variables and constructor to DinerMenuIterator

--- Java/Design-Patterns/Iterator-Pattern-with-Java/src/iterator/DinerMenuIterator.java
+++ Java/Design-Patterns/Iterator-Pattern-with-Java/src/iterator/DinerMenuIterator.java
@@ -1,4 +1,31 @@
 package iterator;
 
-public class DinerMenuIterator {
+import java.util.Iterator;
+
+import menu.MenuItem;
+
+public class DinerMenuIterator implements Iterator<MenuItem> {
+    private MenuItem[] menuItems;
+    private int index;
+
+    public DinerMenuIterator(MenuItem[] menuItems) {
+        this.menuItems = menuItems;
+        index = 0;
+    }
+
+    public boolean hasNext() {
+        boolean hasNext = false;
+
+        if (index < menuItems.length && menuItems[index] != null) {
+            hasNext = true;
+        }
+
+        return hasNext;
+    }
+
+    public MenuItem next() {
+        MenuItem item = menuItems[index];
+        index++;
+        return item;
+    }
 }
