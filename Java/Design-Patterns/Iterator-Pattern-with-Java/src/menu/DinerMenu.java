a1d3986bc7aea91ff6dbc1fbd7403f044a384c05
Brittni Watkins <blwatkins@smu.edu>
2020-05-30 12:16:18 -0500

    Have DinerMenu implement Menu interface. Add instance variables and constructor to DinerMenu class

--- Java/Design-Patterns/Iterator-Pattern-with-Java/src/menu/DinerMenu.java
+++ Java/Design-Patterns/Iterator-Pattern-with-Java/src/menu/DinerMenu.java
@@ -1,4 +1,32 @@
 package menu;
 
-public class DinerMenu {
+import java.util.Iterator;
+
+import iterator.DinerMenuIterator;
+
+public class DinerMenu implements Menu {
+    private static final int MAX_ITEMS = 6;
+    private int numberOfItems;
+    private MenuItem[] menuItems;
+
+    public DinerMenu() {
+        menuItems = new MenuItem[MAX_ITEMS];
+        numberOfItems = 0;
+        addItem("BLT", 2.99);
+        addItem("Soup of the Day", 3.29);
+        addItem("Hot Dog", 3.05);
+    }
+
+    public Iterator<MenuItem> createIterator() {
+        return (new DinerMenuIterator(menuItems));
+    }
+
+    private void addItem(String name, double price) {
+
+        if (numberOfItems < MAX_ITEMS) {
+            MenuItem menuItem = new MenuItem(name, price);
+            menuItems[numberOfItems] = menuItem;
+            numberOfItems++;
+        }
+    }
 }
