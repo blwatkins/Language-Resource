0004c41f838c4d2131c42fca6c7a2355e8522461
Brittni Watkins <blwatkins@smu.edu>
2020-05-30 11:58:13 -0500

    Add instance variables and constructor to Waitress class

--- Java/Design-Patterns/Iterator-Pattern/src/waitress/Waitress.java
+++ Java/Design-Patterns/Iterator-Pattern/src/waitress/Waitress.java
@@ -1,4 +1,30 @@
 package waitress;
 
+import iterator.Iterator;
+import menu.Menu;
+import menu.MenuItem;
+
+import java.util.List;
+
 public class Waitress {
+    List<Menu> menus;
+
+    public Waitress(List<Menu> menus) {
+        this.menus = menus;
+    }
+
+    public void printMenu() {
+
+        for (Menu menu: menus) {
+            printMenu(menu.createIterator());
+        }
+    }
+
+    public void printMenu(Iterator iterator) {
+
+        while (iterator.hasNext()) {
+            MenuItem menuItem = (MenuItem)iterator.next();
+            System.out.println(menuItem.getName() + " -- " + menuItem.getPrice());
+        }
+    }
 }
