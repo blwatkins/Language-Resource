181683103af515cb4ad2dd65fea22996c014275f
Brittni Watkins <blwatkins@smu.edu>
2020-05-30 12:31:28 -0500

    Add attributes and methods to Waitress class

--- Java/Design-Patterns/Iterator-Pattern-with-Java/src/waitress/Waitress.java
+++ Java/Design-Patterns/Iterator-Pattern-with-Java/src/waitress/Waitress.java
@@ -1,4 +1,30 @@
 package waitress;
 
+import java.util.Iterator;
+import java.util.List;
+
+import menu.Menu;
+import menu.MenuItem;
+
 public class Waitress {
+    private List<Menu> menus;
+
+    public Waitress(List<Menu> menus) {
+        this.menus = menus;
+    }
+
+    public void printMenu() {
+
+        for (Menu menu: menus) {
+            printMenu(menu.createIterator());
+        }
+    }
+
+    private void printMenu(Iterator<MenuItem> iterator) {
+
+        while (iterator.hasNext()) {
+            MenuItem menuItem = iterator.next();
+            System.out.println(menuItem.getName() + " -- " + menuItem.getPrice());
+        }
+    }
 }
