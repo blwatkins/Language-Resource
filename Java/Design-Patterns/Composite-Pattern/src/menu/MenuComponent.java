5e4bb8df5866bab29f5183972379fc7eeda7b1d1
Brittni Watkins <blwatkins@smu.edu>
2020-05-30 12:46:34 -0500

    Add attributes and methods to MenuComponent class

--- Java/Design-Patterns/Composite-Pattern/src/menu/MenuComponent.java
+++ Java/Design-Patterns/Composite-Pattern/src/menu/MenuComponent.java
@@ -1,4 +1,29 @@
 package menu;
 
 public class MenuComponent {
+    private String name;
+
+    public MenuComponent(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void add(MenuComponent menuComponent) {
+        throw new UnsupportedOperationException();
+    }
+
+    public void getDescription() {
+        throw new UnsupportedOperationException();
+    }
+
+    public void getPrice() {
+        throw new UnsupportedOperationException();
+    }
+
+    public void print() {
+        throw new UnsupportedOperationException();
+    }
 }
