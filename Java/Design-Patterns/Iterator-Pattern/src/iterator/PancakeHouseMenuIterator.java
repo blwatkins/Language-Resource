ca7bfd47b57d0f6ff9f6fa80994c54f03c677c72
Brittni Watkins <blwatkins@smu.edu>
2020-05-30 11:45:29 -0500

    Have PancakeHouseMenuIterator implement Iterator interface Add instance variables and constructor.

--- Java/Design-Patterns/Iterator-Pattern/src/iterator/PancakeHouseMenuIterator.java
+++ Java/Design-Patterns/Iterator-Pattern/src/iterator/PancakeHouseMenuIterator.java
@@ -1,4 +1,25 @@
 package iterator;
 
-public class PancakeHouseMenuIterator {
+import java.util.List;
+
+import menu.MenuItem;
+
+public class PancakeHouseMenuIterator implements Iterator {
+    private List<MenuItem> menuItems;
+    private int index;
+
+    public PancakeHouseMenuIterator(List<MenuItem> menuItems) {
+        this.menuItems = menuItems;
+        index = 0;
+    }
+
+    public boolean hasNext() {
+        return index < menuItems.size();
+    }
+
+    public MenuItem next() {
+        MenuItem menuItem = menuItems.get(index);
+        index++;
+        return menuItem;
+    }
 }
