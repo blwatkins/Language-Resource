d5d098fd3f1ddf8499e01819d6e6e625c3f31d36
Brittni Watkins <blwatkins@smu.edu>
2020-05-30 12:33:20 -0500

    Add Menu and Waitress creation to Main class

--- Java/Design-Patterns/Iterator-Pattern-with-Java/src/Main.java
+++ Java/Design-Patterns/Iterator-Pattern-with-Java/src/Main.java
@@ -1,6 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import menu.CafeMenu;
+import menu.DinerMenu;
+import menu.Menu;
+import menu.PancakeHouseMenu;
+import waitress.Waitress;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        List<Menu> menus = new ArrayList<>();
+        menus.add(new PancakeHouseMenu());
+        menus.add(new DinerMenu());
+        menus.add(new CafeMenu());
+        Waitress waitress = new Waitress(menus);
+        waitress.printMenu();
     }
 }
