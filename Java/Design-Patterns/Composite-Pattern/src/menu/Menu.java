3a31604453cb4098a7bf2d95bf0fce10acac97ea
Brittni Watkins <blwatkins@smu.edu>
2020-05-30 12:56:59 -0500

    Have Menu class inherit from MenuComponent class. Add instance variables and constructor to Menu class

--- Java/Design-Patterns/Composite-Pattern/src/menu/Menu.java
+++ Java/Design-Patterns/Composite-Pattern/src/menu/Menu.java
@@ -1,4 +1,36 @@
 package menu;
 
-public class Menu {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Menu extends MenuComponent {
+    private List<MenuComponent> menuComponents;
+    private String description;
+
+    public Menu(String name, String description) {
+        super(name);
+        this.description = description;
+        menuComponents = new ArrayList<>();
+    }
+
+    public void add(MenuComponent menuComponent) {
+        menuComponents.add(menuComponent);
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void print() {
+        printNameAndDescription();
+
+        for (MenuComponent menuComponent : menuComponents) {
+            menuComponent.print();
+        }
+    }
+
+    private void printNameAndDescription() {
+        System.out.println(getName() + " -- " + description);
+        System.out.println("---------------------");
+    }
 }
