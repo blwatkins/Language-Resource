2c3222fce2855f8d4eac69563709106919c9e524
Brittni Watkins <blwatkins@smu.edu>
2020-05-30 11:22:52 -0500

    Add instance variables and constructor to MenuItem class

--- Java/Design-Patterns/Iterator-Pattern/src/menu/MenuItem.java
+++ Java/Design-Patterns/Iterator-Pattern/src/menu/MenuItem.java
@@ -1,4 +1,19 @@
 package menu;
 
 public class MenuItem {
+    private String name;
+    private double price;
+
+    public MenuItem(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
 }
