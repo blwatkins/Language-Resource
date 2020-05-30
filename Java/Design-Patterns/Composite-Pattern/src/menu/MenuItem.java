ec166d73c839943da5a25fc77173c191998afb3c
Brittni Watkins <blwatkins@smu.edu>
2020-05-30 12:50:49 -0500

    Have MenuItem extend from MenuComponent. Add instance variables and methods to MenuItem class.

--- Java/Design-Patterns/Composite-Pattern/src/menu/MenuItem.java
+++ Java/Design-Patterns/Composite-Pattern/src/menu/MenuItem.java
@@ -1,4 +1,18 @@
 package menu;
 
-public class MenuItem {
+public class MenuItem extends MenuComponent {
+    private double price;
+
+    public MenuItem(String name, double price) {
+        super(name);
+        this.price = price;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void print() {
+        System.out.println("    " + getName() + " -- " + price);
+    }
 }
