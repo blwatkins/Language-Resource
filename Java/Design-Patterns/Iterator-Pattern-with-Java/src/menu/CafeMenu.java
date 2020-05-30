39943afbc15ad62e9587dcf7749654928ddc4a6a
Brittni Watkins <blwatkins@smu.edu>
2020-05-30 12:28:16 -0500

    Have CafeMenu class implement Menu interface. Add instance variables and constructor to CafeMenu class

--- Java/Design-Patterns/Iterator-Pattern-with-Java/src/menu/CafeMenu.java
+++ Java/Design-Patterns/Iterator-Pattern-with-Java/src/menu/CafeMenu.java
@@ -1,4 +1,23 @@
 package menu;
 
-public class CafeMenu {
+import java.util.*;
+
+public class CafeMenu implements Menu {
+    private Map<String, MenuItem> menuItems;
+
+    public CafeMenu() {
+        menuItems = new HashMap<>();
+        addItem("Soup of the Day", 3.69);
+        addItem("Burrito", 4.29);
+        addItem("Burger", 3.99);
+    }
+
+    public Iterator<MenuItem> createIterator() {
+        return menuItems.values().iterator();
+    }
+
+    private void addItem(String name, double price) {
+        MenuItem menuItem = new MenuItem(name, price);
+        menuItems.put(name, menuItem);
+    }
 }
