b08726e41bf2eef4a7d7fa7af2185dfb9cba8e74
Brittni Watkins <blwatkins@smu.edu>
2020-06-01 14:05:05 -0500

    Add testing examples.

---
+++ Web-Development/JavaScript/_Libraries/Node/LinkedIn-Learning/Node-Essential-Training_Web-Servers-Tests-and-Deployment/Testing/test/index.test.js
@@ -0,0 +1,16 @@
+const appMock = require('../src/app');
+require('../src/index');
+
+jest.mock('../src/app');
+
+describe('test suite', () => {
+
+    it('2 * 2 = 4', () => {
+        expect(2 * 2).toEqual(4);
+    });
+
+    it('calls app.listen()', () => {
+        expect(appMock.listen).toHaveBeenCalled();
+    });
+
+});
