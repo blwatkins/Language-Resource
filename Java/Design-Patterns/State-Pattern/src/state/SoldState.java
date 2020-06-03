100a20e8a4da02f6f739a1b59da13149f92e0a7a
Brittni Watkins <blwatkins@smu.edu>
2020-06-03 08:31:21 -0500

    Add SoldState class

---
+++ Java/Design-Patterns/State-Pattern/src/state/SoldState.java
@@ -0,0 +1,26 @@
+package state;
+
+import gumballmachine.GumballMachine;
+
+public class SoldState extends State {
+
+    public SoldState(GumballMachine gumballMachine) {
+        super(gumballMachine);
+    }
+
+    public void insertQuarter() {
+        System.out.println("Gumball dispensing already in progress");
+    }
+
+    public void ejectQuarter() {
+        System.out.println("Crank has already been turned");
+    }
+
+    public void turnCrank() {
+        System.out.println("Gumball dispensing already in progress");
+    }
+
+    public void dispense() {
+
+    }
+}
