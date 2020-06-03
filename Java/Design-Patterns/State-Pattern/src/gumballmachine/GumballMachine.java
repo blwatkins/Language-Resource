707cbf6a106c583ce37f38839af4c07c73225cde
Brittni Watkins <blwatkins@smu.edu>
2020-06-03 08:37:52 -0500

    Add instance variables to GumballMachine

--- Java/Design-Patterns/State-Pattern/src/gumballmachine/GumballMachine.java
+++ Java/Design-Patterns/State-Pattern/src/gumballmachine/GumballMachine.java
@@ -1,4 +1,17 @@
 package gumballmachine;
 
+import state.State;
+
 public class GumballMachine {
+    private State noQuarterState;
+    private State hasQuarterState;
+    private State soldState;
+    private State soldOutState;
+    private State currentState;
+    private int gumballCount;
+
+    public GumballMachine() {
+
+    }
+
 }
