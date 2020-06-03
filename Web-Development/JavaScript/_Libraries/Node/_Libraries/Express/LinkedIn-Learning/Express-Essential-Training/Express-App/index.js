fc02c14fc52b663fdace9f2c37694dd5cd72c13c
Brittni Watkins <blwatkins@smu.edu>
2020-06-03 11:13:36 -0500

    Add example code from video tutorial.

--- Web-Development/JavaScript/_Libraries/Node/_Libraries/Express/LinkedIn-Learning/Express-Essential-Training/Express-App/index.js
+++ Web-Development/JavaScript/_Libraries/Node/_Libraries/Express/LinkedIn-Learning/Express-Essential-Training/Express-App/index.js
@@ -1,16 +1,14 @@
-import express from 'express';
-import data from './data/data.json';
+import express, { response, request } from 'express';
+import data from './data/data.json'; // data downloaded from https://www.mockaroo.com/
 
 const app = express();
 const PORT = 3000;
 
-app.listen(PORT, () => {
-    console.log(`Your server is running on port ${PORT}`);
-});
-
 // add middleware to the path
 app.use(express.static('public'));
 app.use('/images', express.static('images'));
+// app.use(express.json()); // use json request data
+app.use(express.urlencoded({extended: true})); // use url encoded request data
 
 // get data
 app.get('/', (request, response) => {
@@ -18,15 +16,28 @@ app.get('/', (request, response) => {
 });
 
 app.get('/item/:id', (request, response) => {
+    // begin middleware
     console.log(request.params.id);
     let user = Number(request.params.id);
     console.log(user);
     console.log(data[user - 1]);
+    console.log(`request from ${request.originalUrl}`);
+    console.log(`request method ${request.method}`);
+    // end middleware
     response.json(data[user - 1]);
+});
+
+app.get('/flowerImage', (request, response) => {
+    response.download('./images/flowers_1.jpg'); // download a file to the client
+});
+
+app.get('/flower', (request, response) => {
+    response.redirect('/flowerImage'); // redirect to a site
 });
 
 // add data
 app.post('/newItem', (request, response) => {
+    console.log(request.body);
     response.send(`A POST request with /newItem on port ${PORT}`);
 });
 
@@ -38,4 +49,30 @@ app.put('/item', (request, response) => {
 // remove data
 app.delete('/item', (request, response) => {
     response.send(`A DELETE request with /item on port ${PORT}`);
+});
+
+// chaining routes
+app.route('/person')
+    .get((request, response) => {
+        response.send(`A GET request with /person on port ${PORT}`);
+    })
+    .put((request, response) => {
+        response.send(`A PUT request with /person on port ${PORT}`);
+    })
+    .delete((request, response) => {
+        response.send(`A DELETE request with /person on port ${PORT}`);
+    });
+
+app.get('/error', (request, response) => {
+    throw (new Error());
+});
+
+// error handling middleware
+app.use((error, request, response, next) => {
+    console.error('An error has occured');
+    response.status(500).send('An error has occurred');
+});
+
+app.listen(PORT, () => {
+    console.log(`Your server is running on port ${PORT}`);
 });
