1e7b1cc141a095e0fd8043f77ab2ba5f8c0871c9
Brittni Watkins <blwatkins@smu.edu>
2020-06-03 08:23:02 -0500

    Have NoQuarterState implement State interface.

--- Java/Design-Patterns/State-Pattern/src/state/NoQuarterState.java
+++ Java/Design-Patterns/State-Pattern/src/state/NoQuarterState.java
@@ -1,4 +1,27 @@
 package state;
 
-public class NoQuarterState {
+import gumballmachine.GumballMachine;
+
+public class NoQuarterState implements State {
+    private GumballMachine gumballMachine;
+
+    public NoQuarterState(GumballMachine gumballMachine) {
+        this.gumballMachine = gumballMachine;
+    }
+
+    public void insertQuarter() {
+
+    }
+
+    public void ejectQuarter() {
+        System.out.println("No quarter was inserted into the machine");
+    }
+
+    public void turnCrank() {
+        System.out.println("No quarter was inserted into the machine");
+    }
+
+    public void dispense() {
+        System.out.println("Cannot dispense gumball. No quarter was inserted into the machine");
+    }
 }
