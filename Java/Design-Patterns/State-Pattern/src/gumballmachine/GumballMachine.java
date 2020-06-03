eb781346fe4338a3a3c34bf5600cda309defc840
Brittni Watkins <blwatkins@smu.edu>
2020-06-03 08:48:34 -0500

    Add behavior to GumballMachine class

--- Java/Design-Patterns/State-Pattern/src/gumballmachine/GumballMachine.java
+++ Java/Design-Patterns/State-Pattern/src/gumballmachine/GumballMachine.java
@@ -1,6 +1,6 @@
 package gumballmachine;
 
-import state.State;
+import state.*;
 
 public class GumballMachine {
     private State noQuarterState;
@@ -11,7 +11,60 @@ public class GumballMachine {
     private int gumballCount;
 
     public GumballMachine() {
-
+        noQuarterState = new NoQuarterState(this);
+        hasQuarterState = new HasQuarterState(this);
+        soldState = new SoldState(this);
+        soldOutState = new SoldOutState(this);
+        currentState = soldOutState;
+        gumballCount = 0;
     }
 
+    public State getNoQuarterState() {
+        return noQuarterState;
+    }
+
+    public State getHasQuarterState() {
+        return hasQuarterState;
+    }
+
+    public State getSoldState() {
+        return soldState;
+    }
+
+    public State getSoldOutState() {
+        return soldOutState;
+    }
+
+    public int getGumballCount() {
+        return gumballCount;
+    }
+
+    public void setCurrentState(State currentState) {
+        this.currentState = currentState;
+    }
+
+    public void insertQuarter() {
+        currentState.insertQuarter();
+    }
+
+    public void ejectQuarter() {
+        currentState.ejectQuarter();
+    }
+
+    public void turnCrank() {
+        currentState.turnCrank();
+        currentState.dispense();
+    }
+
+    public void refill(int gumballCount) {
+        this.gumballCount += gumballCount;
+        currentState.refill();
+    }
+
+    public void releaseGumball() {
+
+        if (gumballCount != 0) {
+            gumballCount--;
+        }
+    }
 }
