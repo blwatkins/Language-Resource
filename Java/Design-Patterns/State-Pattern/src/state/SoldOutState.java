b642ab8d07395868fe204cf1f9d798f266d72ead
Brittni Watkins <blwatkins@smu.edu>
2020-06-03 08:35:11 -0500

    Have SoldOutState class inherit from State class

--- Java/Design-Patterns/State-Pattern/src/state/SoldOutState.java
+++ Java/Design-Patterns/State-Pattern/src/state/SoldOutState.java
@@ -1,4 +1,26 @@
 package state;
 
-public class SoldOutState {
+import gumballmachine.GumballMachine;
+
+public class SoldOutState extends State {
+
+    public SoldOutState(GumballMachine gumballMachine) {
+        super(gumballMachine);
+    }
+
+    public void insertQuarter() {
+        System.out.println("Gumball machine is sold out");
+    }
+
+    public void ejectQuarter() {
+        System.out.println("Gumball machine is sold out");
+    }
+
+    public void turnCrank() {
+        System.out.println("Gumball machine is sold out");
+    }
+
+    public void dispense() {
+        System.out.println("Gumball machine is sold out");
+    }
 }
